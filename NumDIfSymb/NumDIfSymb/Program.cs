9c9507ddc9e0f166b49b6a4317535757a72e4e0f
Aleh Rusevich <prokazzaangel@gmail.com>
2022-05-08 16:58:42 +0300

    upadate difnumsymb

---
+++ NumDIfSymb/NumDIfSymb/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NumDIfSymb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            String fromthisstring;
+            int NumDifSymb = 1;
+            int tmp = 0;
+            
+            fromthisstring = Console.ReadLine();
+
+            for (int i = 1; i < fromthisstring.Length; i++)
+            {
+                if (fromthisstring[i-1] != fromthisstring[i])
+                {
+                    NumDifSymb++;
+                    
+                }
+                else
+                {
+                    if (tmp<NumDifSymb)
+                    {
+                        tmp = NumDifSymb;
+                        NumDifSymb = 0;
+                    }
+                    
+                }
+                
+            }
+            if (tmp > NumDifSymb)
+            {
+                Console.WriteLine(tmp);
+            }
+            else
+            {
+                Console.WriteLine(NumDifSymb);
+            } 
+
+        }
+    }
+}
