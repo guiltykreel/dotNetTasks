b92d91af3e9c97e784a040a0ce647ad7bbb16215
Aleh Rusevich <prokazzaangel@gmail.com>
2022-05-27 11:29:41 +0300

    refactored "Counting of different symbols in string"

--- NumDIfSymb/NumDIfSymb/Program.cs
+++ NumDIfSymb/NumDIfSymb/Program.cs
@@ -4,40 +4,49 @@ namespace NumDIfSymb
 {
     class Program
     {
+        /// <summary>
+        /// Count the maximum number of different symbols in string
+        /// </summary>
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello World!");
-            String fromthisstring;
-            int NumDifSymb = 1;
-            int tmp = 0;
             
-            fromthisstring = Console.ReadLine();
+            String FromThisString;
+            int NumberOfDifferentSymbols = 1;
+            int tmp = 0;
+            Console.WriteLine("Input the string");
+            FromThisString = Console.ReadLine();
 
-            for (int i = 1; i < fromthisstring.Length; i++)
+            for (int i = 1; i < FromThisString.Length; i++)
             {
-                if (fromthisstring[i-1] != fromthisstring[i])
+                //for whole string
+                if (FromThisString[i-1] != FromThisString[i])
                 {
-                    NumDifSymb++;
+                    //If previous symbol differ from next 
+                    NumberOfDifferentSymbols++;
                     
                 }
                 else
                 {
-                    if (tmp<NumDifSymb)
+                    if (tmp<NumberOfDifferentSymbols)
                     {
-                        tmp = NumDifSymb;
-                        NumDifSymb = 0;
-                    }
+                        // if next sequens of differ symbols more long than previous
+                        tmp = NumberOfDifferentSymbols;
+                        NumberOfDifferentSymbols = 0;
+                    } 
+                    //else do nothing
                     
                 }
                 
             }
-            if (tmp > NumDifSymb)
+            if (tmp > NumberOfDifferentSymbols)
             {
-                Console.WriteLine(tmp);
+                // if previous sequens of differ symbols more long than next
+                Console.WriteLine($"Maximum nuber of different symbols is {tmp}");
             }
             else
             {
-                Console.WriteLine(NumDifSymb);
+                
+                Console.WriteLine($"Maximum nuber of different symbols is {NumberOfDifferentSymbols}");
             } 
 
         }
