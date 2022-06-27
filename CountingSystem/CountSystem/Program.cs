d6246ff347cdf92d735c828b2d7bd61ead7788dc
Aleh Rusevich <prokazzaangel@gmail.com>
2022-06-27 14:53:10 +0300

    CountingSystem change variables name

--- CountingSystem/CountSystem/Program.cs
+++ CountingSystem/CountSystem/Program.cs
@@ -9,65 +9,65 @@ namespace CountSystem
         /// </summary>
         static void Main()
         {
-            int DecimalNumber; //Convertible number
-            int BaseNumber; // Base of calculation system 
+            int decimalNumber; //Convertible number
+            int baseNumber; // Base of calculation system 
 
             Console.WriteLine("Enter a number in decimal count system: ");
 
-            DecimalNumber = Convert.ToInt32(Console.ReadLine());
+            decimalNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a base of count system: ");
 
-            BaseNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Result = {ToAnotherCountSystem(DecimalNumber, BaseNumber)}");
+            baseNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Result = {ToAnotherCountSystem(decimalNumber, baseNumber)}");
         }
 
-        static string ToAnotherCountSystem(int Decimalnumber, int CountSystemBase)
+        private static string ToAnotherCountSystem(int convertibleDecimalNumber, int countSystemBase)
         {
-            int TemporaryNumber = Decimalnumber;
-            string Result = "";
+            int temporaryNumber = convertibleDecimalNumber;
+            string result = "";
 
-            while (TemporaryNumber > 0) // Convert decimal number into b base calculate system
+            while (temporaryNumber > 0) // Convert decimal number into b base calculate system
             {
                 //reverse of the recording order 
-                switch (TemporaryNumber % CountSystemBase) // if the reminder is 10 or more write symbols
+                switch (temporaryNumber % countSystemBase) // if the reminder is 10 or more write symbols
                 {
                     case 10:
-                        Result = Result.Insert(0, "A");
+                        result = result.Insert(0, "A");
                         break;
                     case 11:
-                        Result = Result.Insert(0, "B");
+                        result = result.Insert(0, "B");
                         break;
                     case 12:
-                        Result = Result.Insert(0, "C");
+                        result = result.Insert(0, "C");
                         break;
                     case 13:
-                        Result = Result.Insert(0, "D");
+                        result = result.Insert(0, "D");
                         break;
                     case 14:
-                        Result = Result.Insert(0, "E");
+                        result = result.Insert(0, "E");
                         break;
                     case 15:
-                        Result = Result.Insert(0, "F");
+                        result = result.Insert(0, "F");
                         break;
                     case 16:
-                        Result = Result.Insert(0, "G");
+                        result = result.Insert(0, "G");
                         break;
                     case 17:
-                        Result = Result.Insert(0, "H");
+                        result = result.Insert(0, "H");
                         break;
                     case 18:
-                        Result = Result.Insert(0, "I");
+                        result = result.Insert(0, "I");
                         break;
                     case 19:
-                        Result = Result.Insert(0, "J");
+                        result = result.Insert(0, "J");
                         break;
                     default:
-                        Result = Result.Insert(0, Convert.ToString(TemporaryNumber % CountSystemBase));
+                        result = result.Insert(0, Convert.ToString(temporaryNumber % countSystemBase));
                         break;
                 }
-                TemporaryNumber = TemporaryNumber / CountSystemBase;
+                temporaryNumber = temporaryNumber / countSystemBase;
             }
-            return Result;
+            return result;
         }
     }
 }
