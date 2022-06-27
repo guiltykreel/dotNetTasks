e2961ecd0aad4ddb592fe29ca5c8f4bd6690b850
Aleh Rusevich <prokazzaangel@gmail.com>
2022-06-27 18:01:08 +0300

    restore autpark

---
+++ AutPark/AutPark/Scooter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutPark
+{
+    class Scooter :Vehicle
+    {
+        internal static string CarType = "Scooter";
+
+        public Scooter() : base(CarType) { }
+
+    }
+}
---
+++ AutPark/AutPark/Transmission.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutPark
+{
+    public class Transmission
+    {
+        public string Type;
+        public int GearNum;
+        public string Manufacturer;
+        public Transmission(object CarType)
+        {
+            switch (CarType)
+            {
+                case "Passenger":
+                    Type = "Automatic";
+                    GearNum = 5;
+                    Manufacturer = "Porshce";
+                    break;
+                case "Cargo":
+                    Type = "Manual";
+                    GearNum = 7;
+                    Manufacturer = "MAN";
+                    break;
+                case "Bus":
+                    Type = "Manual";
+                    GearNum = 6;
+                    Manufacturer = "MAZ";
+                    break;
+                case "Scooter":
+                    Type = "Automatic";
+                    GearNum = 4;
+                    Manufacturer = "Harley Davidson";
+                    break;
+            }
+        }
+
+        public void TransmissionDescription()
+        {
+            Console.WriteLine($"Transmission type: {Type}");
+            Console.WriteLine($"Gear number: {GearNum}");
+            Console.WriteLine($"Manufacturer: {Manufacturer}");
+        }
+    }
+}
