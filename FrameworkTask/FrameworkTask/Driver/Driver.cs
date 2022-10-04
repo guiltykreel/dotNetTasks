279aedc06d7474980c5dc4ec93b8d7d2eb74156a
Aleh Rusevich <prokazzaangel@gmail.com>
2022-10-05 00:11:08 +0300

    fix

--- FrameworkTask/FrameworkTask/Driver/Driver.cs
+++ FrameworkTask/FrameworkTask/Driver/Driver.cs
@@ -22,8 +22,7 @@ namespace FrameworkTask
                         break;
                     default:
                         EdgeOptions edgeOptions = new EdgeOptions();
-                        
-                        edgeOptions.AddArgument("--remote-debugging-port=9222");
+                        edgeOptions.AddArgument("test-type");
                         new DriverManager().SetUpDriver(new EdgeConfig());
                         driver = new EdgeDriver(edgeOptions);
                         break;
