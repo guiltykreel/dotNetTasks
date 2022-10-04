d777e91f1cb3ec684899e2260ef2ec257cf66a7c
Aleh Rusevich <prokazzaangel@gmail.com>
2022-10-04 10:44:17 +0300

    Driver update

--- FrameworkTask/FrameworkTask/Program.cs
+++ FrameworkTask/FrameworkTask/Program.cs
@@ -8,28 +8,30 @@ namespace FrameworkTask
     {
         public static void Main()
         {
-            Environment.SetEnvironmentVariable("Browser", "chrome");
-            Environment.SetEnvironmentVariable("env", "new");
-            string search = "Google Cloud Platform Pricing Calculator";
-            WebDriver driver = new Driver().GetWebDriver();
-            GoogleCloud googleCloud = new GoogleCloud(driver);
-            CloudCalculator cloudCalculator = new CloudCalculator(driver);
-            TempMailPage tempMail = new TempMailPage(driver);
-            Engine engine = new EngineCreator().CreateNewEngine();
+            Console.WriteLine(DateTime.Now.ToString().Replace('.', '_').Replace(' ', '_').Replace(':', '_'));
+            Console.ReadKey();
+            //Environment.SetEnvironmentVariable("Browser", "chrome");
+            //Environment.SetEnvironmentVariable("env", "new");
+            //string search = "Google Cloud Platform Pricing Calculator";
+            //WebDriver driver = new Driver().GetWebDriver();
+            //GoogleCloud googleCloud = new GoogleCloud(driver);
+            //CloudCalculator cloudCalculator = new CloudCalculator(driver);
+            //TempMailPage tempMail = new TempMailPage(driver);
+            //Engine engine = new EngineCreator().CreateNewEngine();
 
-            googleCloud.openPage().
-                //AllowCookies().
-                Search(search).
-                OpenCalculator();
-            cloudCalculator.ComputeEngine(engine).
-               AddToEstimate();
-            tempMail.OpenPage().
-                GetTempEmail();
-            cloudCalculator.SendEstimateToEmail();
-            string cost = cloudCalculator.GetCost().Remove(0, 22);
-            string recievedCost = tempMail.GetMailedCost().Remove(0, 24);
+            //googleCloud.openPage().
+            //    //AllowCookies().
+            //    Search(search).
+            //    OpenCalculator();
+            //cloudCalculator.ComputeEngine(engine).
+            //   AddToEstimate();
+            //tempMail.OpenPage().
+            //    GetTempEmail();
+            //cloudCalculator.SendEstimateToEmail();
+            //string cost = cloudCalculator.GetCost().Remove(0, 22);
+            //string recievedCost = tempMail.GetMailedCost().Remove(0, 24);
             //googleCloud.AcceptCookies.Click();
-            driver.Quit();
+            //driver.Quit();
 
 
 
